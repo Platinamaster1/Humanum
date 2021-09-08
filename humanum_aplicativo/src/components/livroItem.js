9070ee7105bd5bf732992b589e32cf0716f3bf25
Vitor Mugnol <vitormugnol10@gmail.com>
2021-09-08 11:27:15 -0300

    avanços

--- humanum_aplicativo/src/components/livroItem.js
+++ humanum_aplicativo/src/components/livroItem.js
@@ -1,20 +1,31 @@
 import React, {useEffect, useState} from 'react'
 import { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import axios from 'react-native-axios'
 
 export default props => {
     return(
         <View style={styles.container}>
-            <Text>teste</Text>
+            {/* <Text>teste</Text> */}
+            {console.log(props.livro)}
+            {props.livro["capa"]? 
+            (<Image style={styles.livro} source={{uri: props.livro["capa"]}}/>): 
+            (<Text>{props.livro["nome"]}</Text>)}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#FF0000",
-        height: 200,
-        width: 100
-    }
+        margin: 10,
+        // backgroundColor: "#FF0000",
+        height: 180,
+        width: 110
+    },
+    livro: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15
+    },
 })
--- humanum_aplicativo/src/components/recomendacoesTextos.js
+++ humanum_aplicativo/src/components/recomendacoesTextos.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import axios from 'react-native-axios'
 import LivroItem from './livroItem';
 
@@ -11,18 +11,30 @@ export default props => {
         data.forEach(element => {
             // console.log(element)
             linha.push(
-                <LivroItem key={element["id"]}/>
+                <LivroItem key={element["id"]} livro={element}/>
             )
         });
         // console.log(linha)
         return linha
     }
     return(
-        <View>
+        <ScrollView style={styles.container} horizontal={true}>
             {/* <Text>jooj {console.log(props.genero)}</Text> */}
             {/* <Text>{props.genero? props.genero[0]["id"]: console.log("ainda n recebi chefe")}</Text> */}
             {props.genero? linhaTextos(props.genero): console.log("ainda n recebi chefe")}
+            <Text>teste</Text>
+            <Text>teste</Text>
+            <Text>teste</Text>
+            <Text>teste</Text>
             {/* <LivroItem/> */}
-        </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+        backgroundColor: "#dbdbdb",
+        flexDirection: "row"
+    }
+})
